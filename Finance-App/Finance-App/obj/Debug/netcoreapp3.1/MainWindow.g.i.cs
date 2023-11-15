0c22a51d7432faf638202720114ceb33ba3792a6
benne07 <benedigtalt@gmail.com>
2023-11-15 11:34:16 +0100

    update database

--- Finance-App/Finance-App/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
+++ Finance-App/Finance-App/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
@@ -53,7 +53,7 @@ namespace Finance_App {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Finance-App;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Finance-App;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
