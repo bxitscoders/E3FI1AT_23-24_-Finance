59c2a0f89b6affcffd5f9b333ec5b4265565328c
benne07 <benedigtalt@gmail.com>
2024-02-08 10:52:16 +0100

    add GlobalContext

---
+++ FinanceWeb-new/FinanceWeb/Logic/GlobalContext.cs
@@ -0,0 +1,10 @@
+﻿namespace FinanceWeb.Logic
+{
+    public static class GlobalContext
+    {
+        public static int UserId { get; set; }
+        public static string Username { get; set; }
+        public static string FirstName { get; set; }
+        public static string LastName { get; set;}
+    }
+}
